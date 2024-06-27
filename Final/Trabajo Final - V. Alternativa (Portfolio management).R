51ab60696bae97eaf72274ba890dde1d760874f9
Cesar <cr.garciaa@alum.up.edu.pe>
2024-06-27 22:21:44 +0000

    :c

--- Final/Trabajo Final - V. Alternativa (Portfolio management).R
+++ Final/Trabajo Final - V. Alternativa (Portfolio management).R
@@ -1,26 +1,3 @@
----
-title: "Final"
-format: 
-  html:
-    toc: true
-    toc_depth: 6
-    embed-resources: true
-    code-fold: true
-    error: false
-    warning: false
-    code-tools: true
-    code-copy: true
-editor: visual
-output: asis
-theme: cosmo 
----
-
-# 1. Store data
-
-Librerias
-
-```{r}
-#| results: hide
 #libraries-------------
 library(quantmod)
 library(blotter)
@@ -37,37 +14,16 @@ library(purrr)
 library(tseries)
 library(ggplot2)
 library(quantstrat)
-library(ROI)
-library(ROI.plugin.quadprog)
-library(tidyverse)
-library(tseries)
-library(quantmod)
-library(tidyquant)
-library(PortfolioAnalytics)
-library(quantmod)
-library(blotter)
-library(FinancialInstrument)
-library(dplyr)
-library(purrr)
-```
-
-Data
-
-```{r}
-#| results: hide
 #get data --------------------
-from <- "2018-05-31"
-to <- "2024-05-31"
+from <- "2020-07-01"
+to <- "2020-11-13"
 # mySymbols <- c('GOOGL', 'TSLA', 'AMZN', 'AAPL', 'MSFT', 'VOD',  'ADBE', 'NVDA', 'CRM',
 #                'EBAY')
-#mySymbols <- c('GOOGL', 'TSLA')
-mySymbols <- c(tq_index("SP500")$symbol, "SPY")
-mySymbols <- head(mySymbols, 30)
-
+mySymbols <- c('GOOGL', 'TSLA')
 currency("USD")
-#ls_instruments()
+ls_instruments()
 map(mySymbols, ~stock(.x, currency = "USD", multiplier = 1)) #use stock function for each symbol
-#ls_instruments()#check if working
+ls_instruments()#check if working
 #3. Define spreads
 spread_names <- expand.grid(mySymbols, mySymbols) %>%
   filter(Var1 != Var2) %>%
@@ -75,61 +31,24 @@ spread_names <- expand.grid(mySymbols, mySymbols) %>%
   select(spread) %>%
   pull() %>%
   unique()
-map(spread_names, ~FinancialInstrument::spread(.x, "USD", unlist(strsplit(.x, "_")), c(1, -1))) #use spred function for each pair
-
-#ls_instruments()#check if working
+map(spread_names, ~spread(.x, "USD", unlist(strsplit(.x, "_")), c(1, -1))) #use spred function for each pair
+ls_instruments()#check if working
 getSymbols(mySymbols, src = 'yahoo', from = from, to = to)
 map(spread_names, ~{
   pair <- unlist(strsplit(.x, "_"))
   spread <- get(pair[1]) - get(pair[2])
   assign(.x, spread, envir = .GlobalEnv)
 })
-
-```
-
-# 2. Strategy implementation
-
-params
-
-```{r}
-initEq <- 100000
+#Params --------------------------
 window = 60
-pValueTresh  = 0.05
-zscoreThresh = 1
-rankingSDTresh = 10
-
-take_profit <- 0.2
-
-
+pValueTresh  = 0.35
+symb <- spread_names[[1]]
+zscoreThresh = 0.5
+rankingSDTresh = 2
+take_profit <- 0.15 
 stop_loss <- 0.5
-percentageTraded <- 0.1
+initEq = 1000
 
-max_drawdawn = 0.2
-```
-
-Setup
-
-```{r}
-#| results: hide
-# Ensure new environment is clean
-.blotter <- new.env()
-
-currency("USD"); .blotter <- new.env(); .strategy <- new.env(); Sys.setenv(TZ="UTC");Sys.setenv(TZ="UTC"); strategy.st <- portfolio.st <- account.st <- "firststrat";rm.strat(strategy.st)
-initPortf(portfolio.st, symbols = spread_names, initDate = from, currency = "USD")
-initAcct(account.st, portfolios = portfolio.st, initDate = from, currency = "USD", initEq = initEq)
-initOrders(portfolio.st, initDate = from)
-
-
-# Set the timezone to avoid potential issues with date/time
-Sys.setenv(TZ = "UTC")
-# Initialize the environment
-currency("USD")
-```
-
-Get signals
-
-```{r}
-#| results: hide
 #funtions--------------
 cointegrationIndicator <- function(pair_name, n = 60, pTresh) {
   # pTresh= 0.35# pair_name = symb
@@ -260,56 +179,117 @@ add_daily_signals <- function(signals) {
 }
 # Crear el registro de todas las señales
 all_signals_df <- add_daily_signals(signals); all_signals_df$Pair <- gsub("\\.price", "", all_signals_df$Pair)#eliminar .price
-```
 
-Revisar señales
+# portfolio management -------------
+#inicializar portafolio
+currency("USD"); .blotter <- new.env(); .strategy <- new.env(); Sys.setenv(TZ="UTC");Sys.setenv(TZ="UTC"); strategy.st <- portfolio.st <- account.st <- "firststrat";rm.strat(strategy.st)
+initPortf(portfolio.st, symbols = spread_names, initDate = from, currency = "USD")
+initAcct(account.st, portfolios = portfolio.st, initDate = from, currency = "USD", initEq = initEq)
+initOrders(portfolio.st, initDate = from)
 
-```{r}
-head(all_signals_df)
-```
 
-logic for signal application
-
-```{r}
-#| results: hide
-
-library(foreach)
-library(doParallel)
-
-cl <- makeCluster(7)
-registerDoParallel(cl)
-
-dates <- sort(unique(all_signals_df$Date))
-foreach(date = dates) %dopar% {
-
-  #date <- dates[[1]]
+initial_cash <- 1000
+portfolio_state <- data.frame(
+  Date = as.Date('2020-01-01'),
+  InvestedEquity = 0,
+  Available_Cash = initial_cash,
+  Margin_Used = 0,
+  Total_PnL = 0,
+  Pct_Invested = 0,
+  Pct_Margin_Used = 0,
+  Pct_Cash_Available = 1
+)
+portfolio_state$TotalAccountValue <- portfolio_state$InvestedEquity + portfolio_state$Margin_Used + portfolio_state$Available_Cash
+#----------------------
+short_margin_pct <- 0.5
+Date <- seq.Date(from = as.Date(from) - 1, to = as.Date(to), by = "days")
+# Dataframe para almacenar el estado diario del portafolio
+portfolio_state <- data.frame(
+  Date = Date,
+  Equity = rep(0, length(Date)),
+  Available_Cash = rep(0, length(Date)),
+  Margin_Used = rep(0, length(Date)),
+  Total_PnL = rep(0, length(Date)),
+  Pct_Invested = rep(NA, length(Date)),
+  Pct_Margin_Used = rep(NA, length(Date)),
+  Pct_Cash_Available = rep(NA, length(Date)),
+  stringsAsFactors = FALSE
+)
+portfolio_state[1, "Available_Cash"] <- initEq
+# Lista para almacenar las posiciones abiertas
+# Iterar sobre cada día en el dataframe de señales
+for (i in 2:nrow(portfolio_state)) {
+  date <- portfolio_state$Date[i]
   daily_signals <- subset(all_signals_df, Date == date)
-  
-  equity <- getEndEq(Account = account.st, Date = date)
+  if (nrow(daily_signals) == 0) {
+    # Si no hay señales, simplemente copiamos el estado del día anterior
+    portfolio_state[i, ] <- portfolio_state[i - 1, ]
+    portfolio_state[i, "Date"] <- date
+    next
+  }
+  # Capital disponible para asignación
+  available_cash <- portfolio_state$Available_Cash[i - 1]
+  # Proporción de asignación basado en SD inverso
   total_inverse_sd <- sum(1 / daily_signals$SD)
   daily_signals$Weight <- (1 / daily_signals$SD) / total_inverse_sd
-  daily_signals$Assigned_Cash <- daily_signals$Weight * equity*percentageTraded
-  
+  # Asignar capital proporcionalmente
+  daily_signals$Assigned_Cash <- daily_signals$Weight * available_cash
+  margin_used = 0
+  cash_outflows = 0
+  # Procesar cada señal
   for (j in 1:nrow(daily_signals)) {
     pair <- daily_signals$Pair[j]
     entry_type <- daily_signals$EntryType[j]
     price <- daily_signals$Price[j]
     assigned_cash <- daily_signals$Assigned_Cash[j]
     quantity <- floor(assigned_cash / abs(price))
-    
+    if( (price < 0) & (entry_type == "compraSpreadBarato")  | (price > 0) & (entry_type == "vendeSpreadCaro") ){
+      # Ajuste del capital en la cuenta de margen para operaciones en corto
+      margin_used <- margin_used + abs(assigned_cash * short_margin_pct)
+    }
+    if( (price > 0) & (entry_type == "compraSpreadBarato")  | (price < 0) & (entry_type == "vendeSpreadCaro") ){
+      # Ajuste del capital en la cuenta de margen para operaciones en corto
+      cash_outflows <- cash_outflows + assigned_cash
+    }
     if (entry_type == "compraSpreadBarato") {
       addTxn(Portfolio = portfolio.st, Symbol = pair, TxnDate = date, TxnPrice = price, TxnQty = quantity, TxnFees = 0)
     } else if (entry_type == "vendeSpreadCaro") {
       addTxn(Portfolio = portfolio.st, Symbol = pair, TxnDate = date, TxnPrice = price, TxnQty = -quantity, TxnFees = 0)
     }
   }
-  updatePortf(portfolio.st); updateAcct(account.st); updateEndEq(account.st)
+  portfolio_state$Margin_Used[i] <- portfolio_state$Margin_Used[i-1] + margin_used
+  # Actualizar el efectivo disponible después de las asignaciones
+  portfolio_state$Available_Cash[i] <- portfolio_state$Available_Cash[i - 1] - cash_outflows
+  # Actualizar portafolio y cuenta
+  updatePortf(portfolio.st)
+  updateAcct(account.st)
+  updateEndEq(account.st)
+  # Calcular el P&L diario
+  if(date < daily_signals[1,]$Date){
+    equity = 0
+  } else {
+    equity <- getEndEq(Account = account.st, Date = date)
+  }
+  daily_pnl <- equity - portfolio_state$Equity[i - 1]
+  # Actualizar el equity y el efectivo disponible
+  portfolio_state$Equity[i] <- equity
+  #portfolio_state$Available_Cash[i] <- portfolio_state$Available_Cash[i - 1] + daily_pnl
+  portfolio_state$Total_PnL[i] <- portfolio_state$Total_PnL[i - 1] + daily_pnl
+  # Monitorización
+  total_value <- sum(portfolio_state$Equity[i], portfolio_state$Available_Cash[i])
+  portfolio_state$Pct_Invested[i] <- (portfolio_state$Equity[i] - portfolio_state$Available_Cash[i]) / total_value
+  portfolio_state$Pct_Margin_Used[i] <- portfolio_state$Margin_Used[i] / total_value
+  portfolio_state$Pct_Cash_Available[i] <- portfolio_state$Available_Cash[i] / total_value
 }
-```
+View(portfolio_state)
+port <- getPortfolio(portfolio.st)
+dailyTxnPL(Portfolios = portfolio.st)
+getTxns(Portfolio = portfolio.st,Symbol = "GOOGL_TSLA")
+getTxns(Portfolio = portfolio.st,Symbol = "TSLA_GOOGL")
+head(portfolio_state)
+View(portfolio_state)
 
-Stats
 
-```{r}
 #medidas de retorno
 myAccount <- getAccount(account.st)
 myPortfolio <- getPortfolio(portfolio.st)
@@ -324,22 +304,9 @@ t(tStats)
 port.Summ <- myPortfolio$summary
 #returns
 rets <- PortfReturns(Account = account.st)
+ac.ret <- AcctReturns(account.st)
 rownames(rets) <- NULL
 charts.PerformanceSummary(rets, colorset = bluefocus)
 initEq
 getEndEq(account.st,to)
 
-retornosAcumulados <- cumsum(rets)
-account.equity <- getAccount(account.st)$summary$End.Eq
-portfolio.returns <- diff(log(account.equity))
-portfolio.returns <- na.omit(portfolio.returns)
-total_return <- cumsum(portfolio.returns)%>% tail(1)
-
-
-annualized.sd <- StdDev.annualized(portfolio.returns)
-max.drawdown <- maxDrawdown(portfolio.returns)
-sharpe.ratio <- SharpeRatio.annualized(portfolio.returns, Rf = 0.01 / 252) # Asumiendo un 1% de tasa libre de riesgo anual
-
-chart.CumReturns(portfolio.returns, colorset = bluefocus)
-
-```
